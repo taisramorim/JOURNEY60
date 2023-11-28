cfb172ea59c274d5933f0121d4b43d74f7e8ce55
Karoline Ferreira <karoline.a.ferreira@hotmail.com>
2023-11-28 17:42:18 -0300

    Navbar and Career path partially

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,14 @@
 class UsersController < ApplicationController
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to journey_path, notice: 'Your career path has been added!'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
 end
