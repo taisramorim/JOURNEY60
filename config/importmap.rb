1a8f8dc3509b69acdb84fafe9e59a5ea4ff28793
Tais Amorim <rochamorimtais@gmail.com>
2023-12-06 12:09:33 -0300

    Managed to make the checkboxes update without refreshing

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,4 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
+pin "js-confetti", to: "https://ga.jspm.io/npm:js-confetti@0.11.0/dist/es/index.js"
